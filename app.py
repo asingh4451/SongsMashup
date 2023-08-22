394f98ddc43b9a27bcda8237dc20cf39aa4cb770
asingh4451 <108614474+asingh4451@users.noreply.github.com>
2023-08-22 17:38:17 +0530

    try 2

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ def download():
     return send_file(zip_path, as_attachment=True)
 if __name__ == '__main__':
     app.debug = True
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=10000)
